8b40bd535e4280db70b2b22756e5bec652cd9ee6
gerardogilmour <gerardogilmour@outlook.com>
2016-02-01 10:52:57 -0600

    Test commit with github

---
+++ AnimationExampleSolution/AnimationExample/scripts/default.js
@@ -0,0 +1,69 @@
+﻿/// <reference path="jquery-1.12.0.js" />
+
+$(document).on("ready", function () {
+    var milliseconds = 1000;
+    var opacity = 0.5;
+    var worker = null;
+
+    function displayCoverAsync() {
+        return $("#cover").fadeTo(milliseconds, opacity).promise();
+    }
+    
+    function startWorker() {
+        worker = new Worker("scripts/webWorker.js");
+        worker.onmessage = function (e) {
+            $("#time").html(e.data);
+            worker.postMessage("");
+        }
+        worker.postMessage("");
+    }
+
+    function endWorker() {
+        worker.terminate();
+        worker = null;
+    }
+
+    function showMessageContentAsync() {
+        startWorker();
+        $("#messageBox").show();
+        return $("#messageContent").slideDown(milliseconds / 2).promise();
+    }
+
+    function showMessageAsync() {
+        var coverPromise = displayCoverAsync();
+        var messagePromise = coverPromise.pipe(function () {
+            return showMessageContentAsync();
+        });
+        return messagePromise;
+    }
+
+    function displayTimeAsync() {
+        return showMessageAsync();
+    }
+
+
+    function hideMessageContentAsync(message) {
+        var promise = $("#messageContent").slideUp(milliseconds / 2).promise();
+        promise.done(function () {
+            $("#messageBox").hide();
+            endWorker();
+        });
+        return promise;
+    };
+
+    function hideCoverAsync() {
+        return $("#cover").fadeOut(milliseconds).promise();
+    }
+
+    function hideMessageAsync() {
+        var messagePromise = hideMessageContentAsync();
+        var coverPromise = messagePromise.done(function () {
+            return hideCoverAsync();
+        });
+        return coverPromise;
+    }
+
+    $("#showMessageBtn").on("click", displayTimeAsync);
+    $("#closeBtn").on("click", hideMessageAsync);
+    //$("#messageBox").on("click", hideMessageAsync);
+});
---
+++ AnimationExampleSolution/AnimationExample/scripts/webWorker.js
@@ -0,0 +1,13 @@
+﻿function getTime() {
+    var dateTime = new Date();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
+    var seconds = dateTime.getSeconds();
+    var milliseconds = dateTime.getMilliseconds();
+
+    return hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "." + (milliseconds < 1000 ? "0" + (milliseconds < 100 ?  "0" + (milliseconds < 10 ?  "0" + milliseconds : milliseconds) : milliseconds) : milliseconds);
+}
+
+self.onmessage = function(e){
+    postMessage(getTime());
+}
